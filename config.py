cb72080e68e55982b2e442cd16b4e1120068a2f2
SonumMehta <sonum.mehta@theaa.com>
2025-11-28 10:50:44 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 GET_POLICY_DETAILS_URL = "https://api.dev.theaa.digital/v1/policies"
+NEW_PROBLEM_CODES_URL = "https://www-uat.theaa.digital/breakdown-cover/broken-down/api/bcall-service/problems"
+
